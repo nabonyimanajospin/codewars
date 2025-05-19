f70a8249f044a3e305a486293be7bb415c1fb210
nabonyimanajospin <jospinnabonyimana@gmail.com>
2025-05-19 18:06:58 +0200

    Add 8kyu solution: fix-multiply-return.js and even-or-odd.js

---
+++ 8kyu/fix-multiply-return.js
@@ -0,0 +1,4 @@
+// Fix: Added 'return' to output the product of a * b
+function multiply(a, b) {
+  return a * b;
+}
---
+++ 8kyu/odd-or-even.js
@@ -0,0 +1,7 @@
+// even-or-odd.js
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+// Example test
+console.log(evenOrOdd(2)); // "Even"
+console.log(evenOrOdd(3)); // "Odd"
