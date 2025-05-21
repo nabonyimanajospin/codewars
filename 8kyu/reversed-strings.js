73bfff2a7876589efa9bfdd718a05480bf3bc065
nabonyimanajospin <jospinnabonyimana@gmail.com>
2025-05-21 10:13:28 +0200

    Add Codewars 8kyu Boolean to Word solution

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,7 @@
+function solution(str) {
+  return str.split('').reverse().join('');
+}
+
+// Test cases
+console.log(solution('world')); // Output: "dlrow"
+console.log(solution('word'));  // Output: "drow"
