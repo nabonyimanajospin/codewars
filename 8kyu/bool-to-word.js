b20482759b0bd18b63258570e3b5bb155ec177ed
nabonyimanajospin <jospinnabonyimana@gmail.com>
2025-05-22 12:04:32 +0200

    Add new files to kyu8 folder

---
+++ 8kyu/bool-to-word.js
@@ -0,0 +1,7 @@
+// boolean to word
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
+
+console.log(boolToWord(true));  // Output: "Yes"
+console.log(boolToWord(false)); // Output: "No"
---
+++ 8kyu/number-to-string.js
@@ -0,0 +1,10 @@
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+}
+
+// test
+
+console.log(String(4));
+console.log(String(23.34));
+console.log(numberToString(6));
