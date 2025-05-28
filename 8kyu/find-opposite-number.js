e945b9fb1c1e0f15d81c25a9b4965b9c14257fb1
nabonyimanajospin <jospinnabonyimana@gmail.com>
2025-05-28 17:38:22 +0200

    add a file of 8kyu

---
+++ 8kyu/find-opposite-number.js
@@ -0,0 +1,10 @@
+// Function to return the opposite (additive inverse) of a number
+
+function opposite(number) {
+  return -number;
+}
+
+// ✅ Example usage:
+console.log(opposite(1));    // Output: -1
+console.log(opposite(14));   // Output: -14
+console.log(opposite(-34));  // Output: 34
