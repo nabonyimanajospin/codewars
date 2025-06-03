24e99e5117be31b691abafd72c0c00d9271d3484
nabonyimanajospin <jospinnabonyimana@gmail.com>
2025-06-03 14:05:40 +0200

    Adding another file of 8kyu

---
+++ 8kyu/string-to-number.js
@@ -0,0 +1,10 @@
+function stringToNumber(str) {
+  return Number(str);
+}
+
+// Example usage (for testing):
+console.log(stringToNumber("1234"));   // ➜ 1234
+console.log(stringToNumber("605"));    // ➜ 605
+console.log(stringToNumber("1405"));   // ➜ 1405
+console.log(stringToNumber("-7"));     // ➜ -7
+
