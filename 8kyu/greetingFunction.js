ffae5a3a6f85274926f0d7d70f12548349be570c
nabonyimanajospin <jospinnabonyimana@gmail.com>
2025-06-09 12:37:58 +0200

    Adding another file of 8kyu

---
+++ 8kyu/greetingFunction.js
@@ -0,0 +1,7 @@
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+// Examples
+console.log(greet("Jospin"));   // ➞ "Hello, Jospin how are you doing today?"
+console.log(greet("Sarah"));    // ➞ "Hello, Sarah how are you doing today?"
